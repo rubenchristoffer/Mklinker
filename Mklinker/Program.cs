d505041723c437287fc41d69253ad96db445fa1b
Ruben Christoffer <rubenc@protonmail.com>
2020-02-14 10:45:59 +0100

    Initial commit

---
+++ Mklinker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace Mklinker {
+
+	public class Program {
+
+		public static void Main(string[] args) {
+			if (!File.Exists("linker.config")) {
+				Console.WriteLine("Creating config file 'linker.config' since it does not exist");
+				File.Create("linker.config");
+			}
+
+			ExecuteCommands(args);
+		}
+
+		public static void ExecuteCommands (string[] args) {
+
+		}
+
+	}
+
+}
