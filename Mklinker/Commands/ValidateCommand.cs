0b4733d8bfb55b48d563310d82c7b600d3a1bcc4
Ruben Christoffer <rubenc@protonmail.com>
2020-02-24 17:06:00 +0100

    Created Validate command

---
+++ Mklinker/Commands/ValidateCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mklinker.Commands {
+
+    public class ValidateCommand : ICommand {
+
+        public void ExecuteCommand(string[] args) {
+
+        }
+
+        public string GetName() {
+            return "Validate";
+        }
+
+    }
+
+}
