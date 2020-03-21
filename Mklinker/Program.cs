e81ca2fa4ffd361a541e49e389696278cc534825
Ruben Christoffer <rubenc@protonmail.com>
2020-03-21 01:07:08 +0100

    Cleaned up Program.cs before implementing CommandLineParser

--- Mklinker/Program.cs
+++ Mklinker/Program.cs
@@ -8,19 +8,9 @@ namespace Mklinker {
 
 	public class Program {
 
-		public const string VERSION = "v1.1.1";
+		public const string VERSION = "v1.2.0";
 
 		public static Config config { get; private set; }
-
-		private static List<CommandTask> commandTasks = new List<CommandTask>();
-
-		private static ICommand[] availableCommands = new ICommand[] {
-			new LinkAllCommand(),
-			new AddLinkCommand(),
-			new RemoveLinkCommand(),
-			new ListCommand(),
-			new ValidateCommand()
-		};
 
 		public static void Main(string[] args) {
 			if (!File.Exists(Config.configFile)) {
@@ -31,55 +21,8 @@ namespace Mklinker {
 			}
 
 			config = Config.Deserialize(File.ReadAllText(Config.configFile));
-			ParseCommands(args);
-			ExecuteCommands();
-
-			if (commandTasks.Count == 0) {
-				Console.WriteLine("\nHere are the available commands:\n");
-
-				foreach (ICommand command in availableCommands) {
-					Console.WriteLine(command.GetName());
-				}
-			}
 
 			SaveConfig();
-		}
-
-		public static bool IsCommand (string str) {
-			return availableCommands.Any(c => c.GetName().ToLower().Equals(str));
-		}
-
-		public static ICommand GetCommand(string str) {
-			return availableCommands.FirstOrDefault(c => c.GetName().ToLower().Equals(str));
-		}
-
-		public static void ParseCommands(string[] args) {
-			List<int> commandIndexes = new List<int>();
-
-			for (int i = 0; i < args.Length; i++) {
-				if (IsCommand(args[i])) {
-					commandIndexes.Add(i);
-				}
-			}
-
-			for (int x = 0; x < commandIndexes.Count; x++) {
-				int index = commandIndexes[x];
-				int argumentAmount = 0;
-
-				if (x == commandIndexes.Count - 1)
-					argumentAmount = args.Length - index - 1;
-				else
-					argumentAmount = commandIndexes[x + 1] - index - 1;
-
-				string[] commandArguments = new string[argumentAmount];
-				Array.Copy(args, index + 1, commandArguments, 0, argumentAmount);
-
-				commandTasks.Add(new CommandTask(GetCommand(args[index]), commandArguments));
-			}
-		}
-
-		public static void ExecuteCommands() {
-			commandTasks.ForEach(task => task.ExecuteTask());
 		}
 
 		public static void SaveConfig () {
