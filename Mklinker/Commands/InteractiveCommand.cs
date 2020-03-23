086746a9cfd462157572a750733104d1f7f6809b
Ruben Christoffer <rubenc@protonmail.com>
2020-03-23 08:04:57 +0100

    Added ability to use quotes in interactive console

--- Mklinker/Commands/InteractiveCommand.cs
+++ Mklinker/Commands/InteractiveCommand.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CommandLine;
 
 namespace Mklinker.Commands {
 
-	[Verb("interactive", HelpText = "Starts an interactive session where you can run multiple commands without 'Mklinker' in front. Use 'exit' to exit from interactive session.")]
+	[Verb("interactive", HelpText = "Starts an interactive session where you can run multiple commands without 'Mklinker' in front. Use 'exit' to exit from interactive session")]
 	public class InteractiveCommand : IDefaultAction {
 
 		public void Execute() {
@@ -20,7 +21,11 @@ namespace Mklinker.Commands {
 				if (input.ToLower().Equals("exit")) {
 					finished = true;
 				} else {
-					Program.ParseAndExecute(input.Split(' '));
+					string[] args = input.Split('"')
+						.Select((element, index) => index % 2 == 0 ? element.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries) : new string[] { element })
+						.SelectMany(element => element).ToArray();
+
+					Program.ParseAndExecute(args);
 				}
 			}
 		}
