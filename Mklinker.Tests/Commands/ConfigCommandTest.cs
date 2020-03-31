0a63e5fc46ff678fb98fc39c5c3d738ecda42a87
Ruben Christoffer <rubenc@protonmail.com>
2020-03-31 15:35:01 +0200

    Added first test after changes

---
+++ Mklinker.Tests/Commands/ConfigCommandTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mklinker.Abstractions;
+using Mklinker.Commands;
+using NUnit.Framework;
+using System.IO.Abstractions.TestingHelpers;
+using Moq;
+
+namespace Mklinker.Tests.Commands {
+
+	[TestFixture]
+	class ConfigCommandTest {
+
+		[SetUp]
+		public void Setup () {
+			Console.SetOut(TestContext.Out);
+		}
+
+		[Test]
+		public void Execute_CreateConfig_WillSucceed() {
+			const string testPath = "linker.config";
+
+			var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+				{ @"c:\myfile.txt", new MockFileData("Testing is meh.") },
+				{ @"c:\demo\jQuery.js", new MockFileData("some js") },
+				{ @"c:\demo\image.gif", new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) }
+			});
+
+			IConfig defaultConfig = new Config("TestVersion");
+			IConfigHandler configHandler = new ConfigHandler(fileSystem, defaultConfig);
+
+			ConfigCommand configCommand = new ConfigCommand();
+			configCommand.path = testPath;
+			configCommand.create = true;
+
+			configCommand.Execute(configHandler, defaultConfig);
+			Assert.IsTrue(fileSystem.File.Exists(testPath));
+		}
+
+	}
+
+}
