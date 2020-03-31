92b56da7b358a968927cf6de57f01eb6c3e6caf2
Ruben Christoffer <rubenc@protonmail.com>
2020-03-31 17:29:39 +0200

    Added abstraction for Console

---
+++ Mklinker/Abstractions/IConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mklinker.Abstractions {
+
+	interface IConsole {
+
+		void WriteLine(string line);
+		string ReadLine();
+
+	}
+
+}
---
+++ Mklinker/Console.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Mklinker.Abstractions;
+
+namespace Mklinker {
+
+	class Console : IConsole {
+
+		string IConsole.ReadLine() {
+			return System.Console.ReadLine();
+		}
+
+		void IConsole.WriteLine(string line) {
+			System.Console.WriteLine(line);
+		}
+
+	}
+
+}
