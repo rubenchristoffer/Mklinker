83af7891614164ee356a0bfb1e939c2926a23a8c
Ruben Christoffer <rubenc@protonmail.com>
2020-03-31 06:00:48 +0200

    Made Program static

--- Mklinker/Program.cs
+++ Mklinker/Program.cs
@@ -1,22 +1,17 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-using Mklinker.Commands;
-using CommandLine;
-using CommandLine.Text;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Diagnostics;
-using Autofac;
 using System.IO.Abstractions;
+using Autofac;
 
 namespace Mklinker {
 
-	public class Program {
+	public static class Program {
 
-		private static IContainer Container { get; set; }
+		public static string GetVersion() {
+			return FileVersionInfo.GetVersionInfo(Assembly.GetAssembly(typeof (Program)).Location).ProductVersion;
+		}
 
-		Program (string[] args) {
+		public static void Main(string[] args) {
 			var builder = new ContainerBuilder();
 
 			builder.RegisterType<FileSystem>().As<IFileSystem>();
@@ -25,19 +20,9 @@ namespace Mklinker {
 			builder.RegisterType<ArgumentHandler>().As<IArgumentHandler>();
 			builder.RegisterType<CommandExecutor>().As<ICommandExecutor>();
 
-			Container = builder.Build();
-
-			using (var scope = Container.BeginLifetimeScope()) {
+			using (var scope = builder.Build().BeginLifetimeScope()) {
 				scope.Resolve<ICommandExecutor>().Execute(args);
 			}
-		}
-
-		public static string GetVersion() {
-			return FileVersionInfo.GetVersionInfo(Assembly.GetAssembly(typeof (Program)).Location).ProductVersion;
-		}
-
-		public static void Main(string[] args) {
-			new Program(args);
 		}
 
 	}
