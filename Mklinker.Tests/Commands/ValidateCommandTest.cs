8dcac57a7b63daad8755237e542a85f69404725f
Ruben Christoffer <rubenc@protonmail.com>
2020-04-04 19:16:33 +0200

    Added tests for ValidateCommand

--- Mklinker.Tests/Commands/ValidateCommandTest.cs
+++ Mklinker.Tests/Commands/ValidateCommandTest.cs
@@ -1,7 +1,8 @@
-﻿using Moq;
+﻿using System.Collections.Generic;
+using Moq;
 using Mklinker.Commands;
 using Mklinker.Abstractions;
-using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 using NUnit.Framework;
 
 namespace Mklinker.Tests.Commands {
@@ -9,21 +10,83 @@ namespace Mklinker.Tests.Commands {
 	[TestFixture]
 	class ValidateCommandTest {
 
+		TestConsole testConsole;
+		Mock<IConfigHandler> testConfigHandler;
+		MockFileSystem testFileSystem;
+
 		[SetUp]
 		public void Setup () {
+			testConsole = new TestConsole();
+			testConfigHandler = new Mock<IConfigHandler>();
 
+			testFileSystem = new MockFileSystem(new Dictionary<string, MockFileData> {
+				{ @"c:\config.linker", new MockFileData("<?xml version=\"1.0\" encoding=\"utf-16\"?><Config xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" Version=\"v1.1.1\"></Config>") },
+				{ @"c:\invalidconfig.linker", new MockFileData("This content is invalid for a linker file!") },
+				{ @"c:\demo\jQuery.js", new MockFileData("some js") },
+				{ @"c:\demo\image.gif", new MockFileData(new byte[] { 0x12, 0x34, 0x56, 0xd2 }) }
+			});
 		}
 
 		[Test]
-		public void Execute_ () {
+		public void Execute_WithValidConfig_WillPrintValidConfig () {
 			// Arrange
+			ValidateCommand command = new ValidateCommand(false, "testpath");
 
-			
+			List<ConfigLink> links = new List<ConfigLink>();
+
+			var element1 = new ConfigLink(@"c:\config.linker", "target", ConfigLink.LinkType.Symbolic);
+
+			links.Add(element1);
+
+			var mockConfig = new Mock<IConfig>();
+			mockConfig.Setup(m => m.LinkList).Returns(links);
+			testConfigHandler.Setup(m => m.LoadConfig("testpath")).Returns(mockConfig.Object);
+
 			// Act
-
+			((IDefaultCommandHandler)command).Execute(testConsole, testConfigHandler.Object, testFileSystem);
 
 			// Assert
+			Assert.IsTrue(testConsole.GetHistory().Contains("100% valid", System.StringComparison.OrdinalIgnoreCase));
+		}
 
+		[Test]
+		public void Execute_WithEmptyConfig_WillPrintEmptyConfig() {
+			// Arrange
+			ValidateCommand command = new ValidateCommand(false, "testpath");
+
+			List<ConfigLink> links = new List<ConfigLink>();
+
+			var mockConfig = new Mock<IConfig>();
+			mockConfig.Setup(m => m.LinkList).Returns(links);
+			testConfigHandler.Setup(m => m.LoadConfig("testpath")).Returns(mockConfig.Object);
+
+			// Act
+			((IDefaultCommandHandler)command).Execute(testConsole, testConfigHandler.Object, testFileSystem);
+
+			// Assert
+			Assert.IsTrue(testConsole.GetHistory().Contains("empty", System.StringComparison.OrdinalIgnoreCase));
+		}
+
+		[Test]
+		public void Execute_WithInvalidConfig_WillPrintInvalidConfig() {
+			// Arrange
+			ValidateCommand command = new ValidateCommand(false, "testpath");
+
+			List<ConfigLink> links = new List<ConfigLink>();
+
+			var element1 = new ConfigLink(@"c:\config_that_does_not_exist.linker", "target", ConfigLink.LinkType.Symbolic);
+
+			links.Add(element1);
+
+			var mockConfig = new Mock<IConfig>();
+			mockConfig.Setup(m => m.LinkList).Returns(links);
+			testConfigHandler.Setup(m => m.LoadConfig("testpath")).Returns(mockConfig.Object);
+
+			// Act
+			((IDefaultCommandHandler)command).Execute(testConsole, testConfigHandler.Object, testFileSystem);
+
+			// Assert
+			Assert.IsTrue(testConsole.GetHistory().Contains(element1.ToString(), System.StringComparison.OrdinalIgnoreCase));
 		}
 
 	}
--- Mklinker/Commands/ValidateCommand.cs
+++ Mklinker/Commands/ValidateCommand.cs
@@ -11,17 +11,30 @@ namespace Mklinker.Commands {
         [Option ("all", HelpText = "This will list all entries in config and not just the ones that didn't pass validation")]
         public bool displayAll { get; private set; }
 
+        public ValidateCommand() : base() {}
+
+        public ValidateCommand(bool displayAll, string path) : base(path) {
+            this.displayAll = displayAll;
+        }
+
         void IDefaultCommandHandler.Execute(IConsole console, IConfigHandler configHandler, IFileSystem fileSystem) {
             IConfig config = configHandler.LoadConfig(path);
+            bool isValid = true;
 
             foreach (ConfigLink configLink in config.LinkList) {
                 bool validation1 = ValidateExistence(fileSystem, configLink);
                 bool validation2 = ValidateLinkType(fileSystem, configLink);
 
-                if (displayAll || !validation1 || !validation2)
+                if (displayAll || !validation1 || !validation2) {
                     console.WriteLine("\n{0}\n\t# Source path exists: {1}\n\t# Link type acceptable: {2}", configLink.ToString(), validation1 ? "Yes" : "No", validation2 ? "Yes" : "No");
+                }
             }
-        }
+
+            if (config.LinkList.Count == 0)
+                console.WriteLine("Config is empty");
+            else if (isValid)
+                console.WriteLine("Config is 100% valid");
+        }   
 
         internal bool ValidateExistence (IFileSystem fileSystem, ConfigLink configLink) {
             return fileSystem.File.Exists(configLink.sourcePath) || fileSystem.Directory.Exists(configLink.sourcePath);
