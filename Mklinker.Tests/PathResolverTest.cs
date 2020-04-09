e57277cb0b929a69665a3ef5f43d8eac681b7d20
Ruben Christoffer <rubenc@protonmail.com>
2020-04-09 18:14:27 +0200

    Fixed errors for linux build

--- Mklinker.Tests/PathResolverTest.cs
+++ Mklinker.Tests/PathResolverTest.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
-using System.IO.Abstractions.TestingHelpers;
+using System.IO.Abstractions;
 using NUnit.Framework;
 using Mklinker.Commands;
 using Mklinker.Abstractions;
@@ -13,21 +13,22 @@ namespace Mklinker.Tests {
 	class PathResolverTest {
 
 		Mock<IConfig> testConfig;
-		MockFileSystem testFileSystem;
+		Mock<IFileSystem> testFileSystem;
 
 		[SetUp]
 		public void Setup() {
 			testConfig = new Mock<IConfig>();
 			testConfig.Setup(m => m.Variables).Returns(new List<Variable>());
 
-			testFileSystem = new MockFileSystem();
+			testFileSystem = new Mock<IFileSystem>();
 		}
 
 		[Test]
 		public void GetAbsoluteResolvedPath_WithNormalPath_ShouldReturnNormalPath() {
 			// Arrange
 			const string testPath = @"C:/MyPath";
-			PathResolver pathFormatter = new PathResolver(testFileSystem);
+			PathResolver pathFormatter = new PathResolver(testFileSystem.Object);
+			testFileSystem.Setup(m => m.Path.GetFullPath(It.IsAny<string>())).Returns<string>(val => val);
 
 			// Act
 			string result = pathFormatter.GetAbsoluteResolvedPath(testPath, new List<Variable>());
@@ -40,8 +41,9 @@ namespace Mklinker.Tests {
 		public void GetAbsoluteResolvedPath_WithVariablePath_ShouldReturnResolvedPath() {
 			// Arrange
 			string testPath = $@"C:/Users/{ PathResolver.delimiter }User{ PathResolver.delimiter }/Desktop";
-			PathResolver pathFormatter = new PathResolver(testFileSystem);
+			PathResolver pathFormatter = new PathResolver(testFileSystem.Object);
 			var testVariables = new List<Variable>(new Variable[] { new Variable("User", "Frans") });
+			testFileSystem.Setup(m => m.Path.GetFullPath(It.IsAny<string>())).Returns<string>(val => val);
 
 			// Act
 			string result = pathFormatter.GetAbsoluteResolvedPath(testPath, testVariables);
@@ -54,7 +56,8 @@ namespace Mklinker.Tests {
 		public void GetAbsoluteResolvedPath_WithNestedVariablePath_ShouldReturnResolvedPath() {
 			// Arrange
 			string testPath = $@"{ PathResolver.delimiter }UserPath{ PathResolver.delimiter }";
-			PathResolver pathFormatter = new PathResolver(testFileSystem);
+			PathResolver pathFormatter = new PathResolver(testFileSystem.Object);
+			testFileSystem.Setup(m => m.Path.GetFullPath(It.IsAny<string>())).Returns<string>(val => val);
 
 			var testVariables = new List<Variable>(new Variable[] { 
 				new Variable("User", "Frans"),
@@ -72,7 +75,8 @@ namespace Mklinker.Tests {
 		public void GetAbsoluteResolvedPath_WithInvalidNestedVariablePath_ShouldReturnUnresolvedPath() {
 			// Arrange
 			string testPath = $@"{ PathResolver.delimiter }UserPath{ PathResolver.delimiter }";
-			PathResolver pathFormatter = new PathResolver(testFileSystem);
+			PathResolver pathFormatter = new PathResolver(testFileSystem.Object);
+			testFileSystem.Setup(m => m.Path.GetFullPath(It.IsAny<string>())).Returns<string>(val => val);
 
 			var testVariables = new List<Variable>(new Variable[] {
 				new Variable("UserPath", $@"C:/Users/{ PathResolver.delimiter }User{ PathResolver.delimiter }/Desktop")
@@ -89,8 +93,9 @@ namespace Mklinker.Tests {
 		public void GetAbsoluteResolvedPath_WithVariablePathButWithoutVariable_ShouldReturnUnresolvedPath() {
 			// Arrange
 			string testPath = $@"C:/Users/{ PathResolver.delimiter }User{ PathResolver.delimiter }/Desktop";
-			PathResolver pathFormatter = new PathResolver(testFileSystem);
+			PathResolver pathFormatter = new PathResolver(testFileSystem.Object);
 			var testVariables = new List<Variable>();
+			testFileSystem.Setup(m => m.Path.GetFullPath(It.IsAny<string>())).Returns<string>(val => val);
 
 			// Act
 			string result = pathFormatter.GetAbsoluteResolvedPath(testPath, testVariables);
