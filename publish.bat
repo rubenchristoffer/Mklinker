4a1fd978509cc0f4bd840415eb273d44e030b2f5
Ruben Christoffer <rubenc@protonmail.com>
2020-04-12 16:27:14 +0200

    Improved publish batch file

--- publish.bat
+++ publish.bat
@@ -2,11 +2,11 @@
 dotnet clean
 dotnet restore
 cd Mklinker
-dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r win-x86 -c Release /p:PublishSingleFile=true
+dotnet publish -r win-x64 -c Release -o bin/Mklinker-2.0.0-Win64 /p:PublishSingleFile=true
+dotnet publish -r win-x86 -c Release -o bin/Mklinker-2.0.0-Win32 /p:PublishSingleFile=true
 
-dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true
-dotnet publish -r linux-arm -c Release /p:PublishSingleFile=true
+dotnet publish -r linux-x64 -c Release -o bin/Mklinker-2.0.0-Linux64 /p:PublishSingleFile=true
+dotnet publish -r linux-arm -c Release -o bin/Mklinker-2.0.0-LinuxARM /p:PublishSingleFile=true
 
-dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true
+dotnet publish -r osx-x64 -c Release -o bin/Mklinker-2.0.0-OSX64 /p:PublishSingleFile=true
 pause
