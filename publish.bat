90a26ec71b40ec9045a558189ffd0df7c6de8101
Ruben Christoffer <rubenc@protonmail.com>
2020-05-21 13:36:41 +0100

    Changed version to 2.1.0 in publish.bat

--- publish.bat
+++ publish.bat
@@ -4,11 +4,11 @@ dotnet restore
 cd Mklinker
 dotnet pack
 
-dotnet publish -r win-x64 -c Release -o bin/Mklinker-2.0.0-Win64 /p:PublishSingleFile=true
-dotnet publish -r win-x86 -c Release -o bin/Mklinker-2.0.0-Win32 /p:PublishSingleFile=true
+dotnet publish -r win-x64 -c Release -o bin/Mklinker-2.1.0-Win64 /p:PublishSingleFile=true
+dotnet publish -r win-x86 -c Release -o bin/Mklinker-2.1.0-Win32 /p:PublishSingleFile=true
 
-dotnet publish -r linux-x64 -c Release -o bin/Mklinker-2.0.0-Linux64 /p:PublishSingleFile=true
-dotnet publish -r linux-arm -c Release -o bin/Mklinker-2.0.0-LinuxARM /p:PublishSingleFile=true
+dotnet publish -r linux-x64 -c Release -o bin/Mklinker-2.1.0-Linux64 /p:PublishSingleFile=true
+dotnet publish -r linux-arm -c Release -o bin/Mklinker-2.1.0-LinuxARM /p:PublishSingleFile=true
 
-dotnet publish -r osx-x64 -c Release -o bin/Mklinker-2.0.0-OSX64 /p:PublishSingleFile=true
+dotnet publish -r osx-x64 -c Release -o bin/Mklinker-2.1.0-OSX64 /p:PublishSingleFile=true
 pause
