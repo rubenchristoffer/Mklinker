f3bc08f4a1f2a92272c7ed0cd13381c7fbd8df1a
Ruben Christoffer <rubenc@protonmail.com>
2020-05-22 14:02:06 +0100

    Improved color coding for help text and fixed bug

--- Mklinker/ConsoleHelpWriter.cs
+++ Mklinker/ConsoleHelpWriter.cs
@@ -1,50 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.IO;
-using System.IO.Enumeration;
+﻿using System.IO;
+using System.Linq;
 
 namespace Mklinker {
 
 	class ConsoleHelpWriter : StringWriter {
 
-		/**
-		 * NOTE: Don't look too much into this messy function.
-		 * All it does is set the color codes in the help screen
-		 */
 		public override void Write (string value) {
-			// Colors Mklinker vX.X.X
-			System.Console.ForegroundColor = ConsoleColor.Yellow;
+			string[] lines = value.Split (System.Console.Out.NewLine);
 
-			for (int i = 0; i < value.Length; i++) {
-				if (value[i] == '\n') {
-					// Want to color both error AND error message
-					// Keeps color until carriage return (13) is found
-					if (value.Length > i + 1) {
-						if (value[i + 1] == 13) {
-							System.Console.ResetColor ();
-						}
-					}
+			for (int i = 0; i < lines.Length - 1; i++) {
+				WriteLine (lines[i]);
+			}
+		}
 
-					if (value.Length > i + 6 && value.Substring (i, 6).Contains ("error", StringComparison.OrdinalIgnoreCase)) {
-						// Error = Red
-						System.Console.ForegroundColor = ConsoleColor.Red;
-					} else if (value.Length > i + 10 && value.Substring (i, 10).Contains ("copyright", StringComparison.OrdinalIgnoreCase)) {
-						// Header = Yellow
-						System.Console.ForegroundColor = ConsoleColor.Yellow;
-					} else if (value.Length > i + 9 && value.Substring (i, 9).Contains ("help", StringComparison.OrdinalIgnoreCase)) {
-						// Help/Version text = White
-						System.Console.ForegroundColor = ConsoleColor.White;
-					} else if (value.Length > i + 12 && value.Substring (i, 12).Contains ("version", StringComparison.OrdinalIgnoreCase)) {
-						// Help/Version text = White
-						System.Console.ForegroundColor = ConsoleColor.White;
-					}
-				}
+		public override void WriteLine (string value) {
+			// Use prefixes to add color coding to certain lines
+			string[] yellowPrefixes = { "Mklinker", "Copyright" };
+			string[] whitePrefixes = { "help", "--help", "version", "--version" };
+			string[] redPrefixes = { "ERROR", "Verb", "Option", "A required" };
 
-				System.Console.Out.Write (value[i]);
+			if (yellowPrefixes.Any (p => value.Trim ().StartsWith (p))) {
+				System.Console.ForegroundColor = System.ConsoleColor.Yellow;
 			}
 
-			// Reset color back to default at the end
+			if (whitePrefixes.Any (p => value.Trim ().StartsWith (p))) {
+				System.Console.ForegroundColor = System.ConsoleColor.White;
+			}
+
+			if (redPrefixes.Any (p => value.Trim ().StartsWith (p))) {
+				System.Console.ForegroundColor = System.ConsoleColor.Red;
+			}
+
+			System.Console.Out.WriteLine (value);
 			System.Console.ResetColor ();
 		}
 
