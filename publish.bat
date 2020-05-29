5ae8a45ec3c805d96916afba7367bcb32e0fc616
Ruben Christoffer <rubenc@protonmail.com>
2020-05-29 20:59:49 +0200

    Added version variable to publish.bat

--- publish.bat
+++ publish.bat
@@ -1,14 +1,16 @@
 @echo off
+set version=2.1.3
+
 dotnet clean
 dotnet restore
 cd Mklinker
 dotnet pack
 
-dotnet publish -r win-x64 -c Release -o bin/Mklinker-2.1.3-Win64 /p:PublishSingleFile=true
-dotnet publish -r win-x86 -c Release -o bin/Mklinker-2.1.3-Win32 /p:PublishSingleFile=true
+dotnet publish -r win-x64 -c Release -o bin/Mklinker-%version%-Win64 /p:PublishSingleFile=true
+dotnet publish -r win-x86 -c Release -o bin/Mklinker-%version%-Win32 /p:PublishSingleFile=true
 
-dotnet publish -r linux-x64 -c Release -o bin/Mklinker-2.1.3-Linux64 /p:PublishSingleFile=true
-dotnet publish -r linux-arm -c Release -o bin/Mklinker-2.1.3-LinuxARM /p:PublishSingleFile=true
+dotnet publish -r linux-x64 -c Release -o bin/Mklinker-%version%-Linux64 /p:PublishSingleFile=true
+dotnet publish -r linux-arm -c Release -o bin/Mklinker-%version%-LinuxARM /p:PublishSingleFile=true
 
-dotnet publish -r osx-x64 -c Release -o bin/Mklinker-2.1.3-OSX64 /p:PublishSingleFile=true
+dotnet publish -r osx-x64 -c Release -o bin/Mklinker-%version%-OSX64 /p:PublishSingleFile=true
 pause
