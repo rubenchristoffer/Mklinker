da23a55d1c5643776d27e52d4bd22dd6b6d58f3c
Ruben Christoffer <rubenc@protonmail.com>
2020-05-29 20:57:31 +0200

    Added publish and test scripts for unix

---
+++ publish.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+version=2.1.3
+
+dotnet clean
+dotnet restore
+cd Mklinker
+dotnet pack
+
+dotnet publish -r win-x64 -c Release -o bin/Mklinker-$version-Win64 /p:PublishSingleFile=true
+dotnet publish -r win-x86 -c Release -o bin/Mklinker-$version-Win32 /p:PublishSingleFile=true
+
+dotnet publish -r linux-x64 -c Release -o bin/Mklinker-$version-Linux64 /p:PublishSingleFile=true
+dotnet publish -r linux-arm -c Release -o bin/Mklinker-$version-LinuxARM /p:PublishSingleFile=true
+
+dotnet publish -r osx-x64 -c Release -o bin/Mklinker-$version-OSX64 /p:PublishSingleFile=true
---
+++ runtests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dotnet restore
+dotnet build
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
+dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.5.3
+reportgenerator "-reports:Mklinker.Tests/coverage.opencover.xml" "-targetdir:gh-pages/CoverageReport" -reporttypes:Html
